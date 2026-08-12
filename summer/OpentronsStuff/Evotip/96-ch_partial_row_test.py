fd968b7b599505d39b78693a7324d78b9af81a7d
Edlyn To <68762251+EdlynTo@users.noreply.github.com>
2026-08-12 10:39:15 -0400

    row 96 chan pick up

--- summer/OpentronsStuff/Evotip/96-ch_partial_row_test.py
+++ summer/OpentronsStuff/Evotip/96-ch_partial_row_test.py
@@ -1,15 +1,15 @@
 # Version 1 - August 21, 2024
 
 from opentrons import protocol_api
-from opentrons.protocol_api import COLUMN, ALL
+from opentrons.protocol_api import ROW, ALL
 from opentrons import types
 # points syntax
 #p96.aspirate(100, well.bottom(2).move(types.Point(x=-2, y=2, z =-1)))
 import math
 
 metadata = {
-    'protocolName': 'Partial Tip Pick Up',
-    'description': 'Testing partial tip pick up and return with 96 channel pipette'
+    'protocolName': 'Partial Row Tip Pick Up',
+    'description': 'Testing partial row tip pick up and return with 96 channel pipette'
 }
 requirements = {"robotType": "Flex", "apiLevel": "2.28"}
 
@@ -28,52 +28,27 @@ def add_parameters(parameters: protocol_api.Parameters):
     )
 
     parameters.add_int(
-        display_name="How many columns to run?",
-        variable_name="columns",
-        default=3,minimum=1,maximum=12,
-        description="Select total columns to pick up, one at a time from the tiprack.")
+        display_name="How many rows to run?",
+        variable_name="rows",
+        default=3,minimum=1,maximum=8,
+        description="Select total rows to pick up, one at a time from the tiprack.")
 
     parameters.add_int(
-        display_name="Empty columns",
+        display_name="Empty rows",
         variable_name="skip",
-        description="Select the number of empty tip columns to skip before picking up tips",
+        description="Select the number of empty tip rows to skip before picking up tips",
         default=0,
         choices=[
-                {"display_name": "0", "value": 0},
-                {"display_name": "1", "value": 1},
-                {"display_name": "2", "value": 2},
-                {"display_name": "3", "value": 3},
-                {"display_name": "4", "value": 4},
-                {"display_name": "5", "value": 5},
-                {"display_name": "6", "value": 6},
-                {"display_name": "7", "value": 7},
-                {"display_name": "8", "value": 8},
-                {"display_name": "9", "value": 9},
-                {"display_name": "10", "value": 10},
-                {"display_name": "11", "value": 11}
-                ]
+            {"display_name": "0", "value": 0},
+            {"display_name": "1", "value": 1},
+            {"display_name": "2", "value": 2},
+            {"display_name": "3", "value": 3},
+            {"display_name": "4", "value": 4},
+            {"display_name": "5", "value": 5},
+            {"display_name": "6", "value": 6},
+            {"display_name": "7", "value": 7},
+            ]
         )
-
-    # parameters.add_str(
-    #         display_name="Empty columns",
-    #         variable_name="start",
-    #         description="Select the number of empty tip columns to skip before picking up tips",
-    #         default="A1",
-    #         choices=[
-    #                 {"display_name": "0", "value": "A1"},
-    #                 {"display_name": "1", "value": "A2"},
-    #                 {"display_name": "2", "value": "A3"},
-    #                 {"display_name": "3", "value": "A4"},
-    #                 {"display_name": "4", "value": "A5"},
-    #                 {"display_name": "5", "value": "A6"},
-    #                 {"display_name": "6", "value": "A7"},
-    #                 {"display_name": "7", "value": "A8"},
-    #                 {"display_name": "8", "value": "A9"},
-    #                 {"display_name": "9", "value": "A10"},
-    #                 {"display_name": "10", "value": "A11"},
-    #                 {"display_name": "11", "value": "A12"}
-    #                 ]
-    #         )
 
 def run(protocol: protocol_api.ProtocolContext):
 
@@ -85,19 +60,19 @@ def run(protocol: protocol_api.ProtocolContext):
 
 
     tips = protocol.load_labware(tiprack, "C2", label = rack)
-    used_tips = protocol.load_labware(tiprack, "B2", label = f"{rack} (returned)")
+    used_tips = protocol.load_labware(tiprack, "C3", label = f"{rack} (returned)")
 
     used_tips.set_empty()
 
     p96 = protocol.load_instrument('flex_96channel_1000', 'left')
 
     p96.configure_nozzle_layout(
-        style=COLUMN,
-        start="A1",
+        style=ROW,
+        start="H1",
         tip_racks=[tips, used_tips])
 
-    for i in range(protocol.params.columns):
-        p96.pick_up_tip(tips[f"A{12 - protocol.params.skip - i}"])
+    rows = "ABCDEFGH"
+    for i, rowLetter in enumerate(rows[protocol.params.skip:protocol.params.skip+protocol.params.rows]):
+        p96.pick_up_tip(tips[f"{rowLetter}1"])
         protocol.delay(1)
-        p96.drop_tip(used_tips[f"A{protocol.params.skip + i + 1}"])
-    
+        p96.drop_tip(used_tips[f"{rows[protocol.params.skip + i]}1"])
